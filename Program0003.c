8e3a01e321a1244bfbb7b1e4e036073a26e35c4d
Madhur Taneja <tanejamadhur96@gmail.com>
2017-07-17 23:20:50 +0530

    Create Program0003.c

---
+++ Program0003.c
@@ -0,0 +1,44 @@
+Ques:
+WAP to check if number is prime or not but by optimizing the code to reduce the time complexity.
+
+CODE:
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+        int i, n, flag = 0, sr=0;
+        printf("Enter a number to check if it is prime or not\n");
+        scanf("%d", &n);
+
+        sr = sqrt(n);
+        if( n % 2 != 0)
+        {
+                for(i=3;i<=sr;)
+                {
+                        if(n % i != 0)
+                        {
+                                flag = 1;
+                        }
+                        else
+                        {
+                                flag = 0;
+                                break;
+                        }
+                        i += 2;
+                }
+                if(flag == 1)
+                {
+                        printf("Number is prime\n");
+                }
+                else
+                {
+                        printf("Number is not prime\n");
+                }
+        }
+        else
+        {
+                printf("Number is not a prime\n");
+        }
+}
