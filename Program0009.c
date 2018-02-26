9b7aa92d8cb26936f2b0052b49289063ed674c59
Madhur Taneja <tanejamadhur96@gmail.com>
2018-02-26 20:12:01 +0530

    Print in Reverse

--- Program0009.c
+++ Program0009.c
@@ -1,8 +1,20 @@
 //Ques:
-//Print in Reverse (Hackerrank Data Structures)
+//You are given the pointer to the head node of a linked list and you need to print all its elements in reverse order from tail to head,
+//one element per line. The head pointer may be null meaning that the list is empty - in that case, do not print anything! 
+//Hackerrank (Data Structures->Linked List->Print in Reverse)
 
 //CODE:
 
+/*
+  Print elements of a linked list in reverse order as standard output
+  head pointer could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
 void ReversePrint(Node *head)
 {
   // This is a "method-only" submission. 
@@ -17,6 +29,4 @@ void ReversePrint(Node *head)
         ReversePrint(head->next);
         cout << head->data << endl;
     }
-   
-
 }
